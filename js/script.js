49a564554bbfbf02653ffee6f2c9352efa5ad238
loadlin2 <load.lin@yahoo.com>
2018-08-07 21:22:06 +0300

    qq

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log('u a ' + window.location);
+
+const bl=document.querySelector('.imageBlock');
+let answer=parseFloat(prompt("give me number"));
+(answer<0) ? console.log(answer * (-1)) : console.log(answer);
